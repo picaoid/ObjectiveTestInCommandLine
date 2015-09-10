b0dbb89926cc81df6b7571e5b264b2c0f47db084
Coşkun Pınar <pica@PINARS.local>
2015-09-10 13:25:28 +0300

    Initial Commit

---
+++ ObjectiveTestInCommandLine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveTestInCommandLine
+//
+//  Created by  Coşkun Pınar on 10.09.2015.
+//  Copyright (c) 2015  Coşkun Pınar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
